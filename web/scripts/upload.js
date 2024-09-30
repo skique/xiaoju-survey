f923095e953726ea8475435d410c35932aac175a
dayou <853094838@qq.com>
2024-09-30 17:45:15 +0800

    upload assets

---
+++ web/scripts/upload.js
@@ -0,0 +1,55 @@
+import * as path from 'path'
+import {globby} from 'globby';
+
+
+import { Client } from 'minio';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import fs from 'fs'
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const uploadFile = async ({
+  file,
+  filepath
+}) =>{
+  const config ={
+    ACCESS_KEY: 'AKDD000000000011BXDJURJBBCNHUJ', // your_access_key
+    SECRET_KEY: 'ASDDLtlXpdVhsIkapFEevIjtGNDshnAxpNFtjHCJ', // your_secret_key
+    BUCKET: 'xiaojuwenjuan', // your_bucket
+    REGION: 'hangzhou',
+    ENDPOINT: 's3-gzpu.didistatic.com', // endpoint
+    USE_SSL: true, // useSSL
+  }
+  const client = new Client({
+    endPoint: config.ENDPOINT,
+    accessKey: config.ACCESS_KEY,
+    secretKey: config.SECRET_KEY,
+    region:config.REGION,
+    useSSL: config.USE_SSL,
+    pathStyle: true,
+  });
+  const key =  'surveyUpload/dist/' + filepath
+  await client.putObject(config.BUCKET, key, file);
+}
+
+const upload = async () => {
+  const filesPath = path.join(__dirname, '../dist/')
+  const files = await globby(filesPath, {
+    ignore: ['**/*.html'],
+  })
+  const tasks = files.map(file => {
+    return () =>
+      uploadFile({
+        file: fs.createReadStream(file),
+        filepath: file.replace(filesPath, ''),
+      })
+  })
+
+  await Promise.all(tasks.map(task => task()))
+
+  console.log('静态资源上传成功：', JSON.stringify(files, null, 2))
+}
+
+upload()
--- web/vite.config.ts
+++ web/vite.config.ts
@@ -49,7 +49,9 @@ const mpaPlugin = createMpaPlugin({
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
+  base: process.env.NODE_ENV === 'production'
+  ? 'https://s3-gzpu.didistatic.com/xiaojuwenjuan/surveyUpload/dist/'
+  : '/',
   optimizeDeps: {
     include: [
       'lodash-es',
